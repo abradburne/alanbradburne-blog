8b51f82dcac3f7d57b0219a3e58da7eabb60bef7
Alan Bradburne <=>
2013-05-19 14:55:24 +0900

    initial commit

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/alanbradburne-blog/"
+http_images_path = "/alanbradburne-blog/images"
+http_fonts_path = "/alanbradburne-blog/fonts"
+css_dir = "public/alanbradburne-blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
